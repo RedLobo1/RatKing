c4de1b3ab1ad9cbc4e1b8ddbbed69b8b57feb44e
VervaekeMilan <milan.vervaeke@student.howest.be>
2025-02-12 16:24:07 +0100

    Fixing teleportation for a non-single-tile blob

--- RatKingUnity/Assets/Scripts/SewerTeleportation.cs
+++ RatKingUnity/Assets/Scripts/SewerTeleportation.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 public class SewerTeleportation : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _TargetTile;
+    private GameObject _connectedSewer;
 
     [SerializeField]
-    private GameObject _connectedSewer;
+    private byte _SewerSize;
     private void Start()
     {
 
@@ -19,20 +19,90 @@ public class SewerTeleportation : MonoBehaviour
         if (Physics.Raycast(position, Vector3.up, out hit, 1))
         {
             //if(hit.collider.CompareTag("BlobConnected") ||) // move specific things
+
             if (hit.collider != null)
             {
-                Debug.Log("Teleporting");
-                hit.collider.transform.position = new Vector3(_TargetTile.transform.position.x, hit.collider.transform.position.y, _TargetTile.transform.position.z);
-                float RotationAngle = Vector3.Angle(this.transform.forward, _connectedSewer.transform.forward);
-                hit.collider.transform.rotation = Quaternion.Euler(0, hit.collider.transform.rotation.y + RotationAngle, 0); //Rotating the object
-
-                var blob = hit.collider.transform.parent;
-                if (blob.tag == "BlobConnected") //Removing parency if a connected block teleported
+                if (hit.collider.transform.parent == null) //player has no parent to for now this is the player
                 {
-                    blob.transform.parent = null;
-                    blob.tag = "BlobDisconnected";
+                    Teleport(hit.collider.transform, Vector3.zero);
                 }
+                else //this is a blob
+                {
+                    var blob = hit.collider.transform.parent;
+                    //if (blob.transform.childCount == 1) //single blob always fir in sewer
+                    //{
+                    //    Teleport(hit.collider.transform.parent);
+                    //    DisconnectBlob(blob);
+                    //}
+                    //else //can teleport only if size fits in sewer
+                    //{
+                    var mask = false;
+                    if (this.transform.rotation.eulerAngles.y % 180 == 0)//sewer is rotated 180 degrees
+                        mask = GetColliderSize(blob).x <= _SewerSize; //check x size
+                    else if (this.transform.rotation.eulerAngles.y % 180 == 90) //sewer is rotated 90 degrees
+                        mask = GetColliderSize(blob).z <= _SewerSize; //check z size
+
+
+                    if (mask)
+                    {
+                        Teleport(hit.collider.transform.parent, hit.collider.transform.localPosition);
+                        DisconnectBlob(blob);
+                    }
+                    //}
+                }
+
+
             }
         }
+    }
+
+    private Vector3Int GetColliderSize(Transform blob)
+    {
+        Bounds totalBounds = new Bounds(blob.GetChild(0).position, Vector3.zero);
+        foreach (Transform child in blob)
+        {
+            Collider childCollider = child.GetComponent<Collider>();
+            if (childCollider != null)
+            {
+                totalBounds.Encapsulate(childCollider.bounds);
+            }
+        }
+        return Vector3Int.RoundToInt(totalBounds.size);
+        // Output the total size of the parent (based on the children's colliders)
+    }
+
+    private void DisconnectBlob(Transform blob)
+    {
+        if (blob.tag == "BlobConnected") //Removing parency if a connected block teleported
+        {
+            blob.transform.parent = null;
+            blob.tag = "BlobDisconnected";
+        }
+    }
+
+    private void Teleport(Transform transform, Vector3 offset)
+    {
+        Debug.Log("Teleporting");
+
+        if (transform.rotation.eulerAngles.y == 180)
+        {
+            offset *= -1;
+        }
+
+        //if (this.transform.rotation.y == 180)
+        transform.position = new Vector3(_connectedSewer.transform.position.x - _connectedSewer.transform.forward.x + offset.x,
+                                             transform.position.y,
+                                             _connectedSewer.transform.position.z - _connectedSewer.transform.forward.z + offset.z);
+
+        //else
+
+        //transform.position = new Vector3(_connectedSewer.transform.position.x - _connectedSewer.transform.forward.x,
+        //                             transform.position.y,
+        //                             _connectedSewer.transform.position.z - _connectedSewer.transform.forward.z);
+
+        float RotationAngle = Vector3.Angle(this.transform.forward, _connectedSewer.transform.forward);
+        transform.rotation = Quaternion.Euler(0, Mathf.RoundToInt(transform.rotation.eulerAngles.y + RotationAngle), 0); //Rotating the object
+
+
     }
 }

fd92fe959ac26e90f5d250c6314a7a9591d4fdd0
VervaekeMilan <milan.vervaeke@student.howest.be>
2025-02-12 09:25:19 +0100

    Imported test scene movement and grid

---
+++ RatKingUnity/Assets/Scripts/TileProperties.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TileProperties : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public ETileType TileType;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
